8172d8c55e532b309fc8d49724ede1fe1a61adaa
Meed_Man <112403755+Mede1x@users.noreply.github.com>
2024-04-03 17:43:38 +0100

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,5 +1,5 @@
 from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
-
+from pathlib import Path
 
 OUTPUT_PATH = Path(__file__).parent
 ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\Meed\Desktop\Projects\11\build\assets\frame0")
